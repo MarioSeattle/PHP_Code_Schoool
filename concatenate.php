a2baf72f016c6ea2550807ec2e94dd75d1c53cea
MarioSeattle <marioseattle@live.com>
2016-12-04 19:39:09 -0800

    Concatenate dot =

--- concatenate.php
+++ concatenate.php
@@ -1,7 +1,11 @@
 <?php
 $name = 'mario';
 $string_one = "Learning how to display \"hello $name!\"to the screen.";
-$string_one = 'Learning how to display "hello' . $name . '!" to the screen.' . "\n";
+$string_one = 'Learning how to display';
+$string_one .= '"hello';
+$string_one .= $name;
+$string_one .= '!" to the screen.';
+$string_one .= "\n";
 echo $string_one;
 
 ?>
