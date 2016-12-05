839b0b62f9582df9b6f2848ff76ee58f093be81d
MarioSeattle <marioseattle@live.com>
2016-12-04 19:21:04 -0800

    The dot allowed combining strings

--- concatenate.php
+++ concatenate.php
@@ -1,7 +1,7 @@
 <?php
 $name = 'mario';
 $string_one = "Learning how to display \"hello $name!\"to the screen.";
-$string_one = "Learning how to display \"hello $name!\"to the screen.";
+$string_one = 'Learning how to display "hello' . $name . '!" to the screen.' . "\n";
 echo $string_one;
 
 ?>
