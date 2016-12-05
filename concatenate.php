a711089b74959f8420410f21b650a359d7025dce
MarioSeattle <marioseattle@live.com>
2016-12-04 19:15:28 -0800

    Concatenation 101

---
+++ concatenate.php
@@ -0,0 +1,7 @@
+<?php
+$name = 'mario';
+$string_one = "Learning how to display \"hello $name!\"to the screen.";
+$string_one = "Learning how to display \"hello $name!\"to the screen.";
+echo $string_one;
+
+?>
