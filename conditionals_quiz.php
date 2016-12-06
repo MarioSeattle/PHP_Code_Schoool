543212ab138af46f3908459303407d3b34ebff05
MarioSeattle <marioseattle@live.com>
2016-12-06 14:15:31 -0800

    GPA and Honor Roll

--- conditionals_quiz.php
+++ conditionals_quiz.php
@@ -8,6 +8,16 @@ $studentOneGPA = 3.8;
 $studentTwoName = 'Treasure';
 $studentTwoGPA = 4.0;
 
+if($studentOneGPA == 4.0) {
+    echo $studentOneName . 'made the Honor Roll';
+}else{
+    echo $studentOneName . 'has a GPA of ' . "$studentOneGPA";
+}
+if($studentTwoGPA == 4.0) {
+    echo $studentTwoName .'made the Honor Roll';
+}else{
+    echo $studentTwoName . 'has a GPA of' . "$studentTwoGPA";
+}
 
 
 
