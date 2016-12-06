8cb3f5786e49995777f858fff5cfd09300ee0b73
MarioSeattle <marioseattle@live.com>
2016-12-06 12:03:58 -0800

    Change the string = false (no text)

--- conditional_statements.php
+++ conditional_statements.php
@@ -24,7 +24,7 @@ $b = '10';
 //var_dump($a === $b);
 
 //var_dump($string_one == 'Learning to display "Hello Mario!" to the screen.');
-if ($string_one == 'Learning to display "Hello Mario!" to the screen.'){
+if ($string_one == 'Learning to display "Hello Mario!" to the screen'){
     echo 'the values match';
 }
 
