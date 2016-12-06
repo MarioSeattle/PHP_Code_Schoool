cd8d821307416ef9831317fb5acf03217fc0cbe2
MarioSeattle <marioseattle@live.com>
2016-12-05 17:53:19 -0800

    == equals === identical

---
+++ conditional_statements.php
@@ -0,0 +1,35 @@
+<?php
+
+<?php
+$name = 'mario';
+$string_one = "Learning how to display \"hello $name!\"to the screen.";
+$string_one = 'Learning how to display';
+$string_one .= '"hello';
+$string_one .= $name;
+$string_one .= '!" to the screen.';
+$string_one = $string_one . "\n";
+
+$string_one = 'I am' . $string_one;
+//echo $string_one;
+
+$isReady = true;
+//var_dump($isReady);
+$isReady = false;
+//var_dump($isReady);
+//One plus the string 2 ""
+var_dump(1 + "2");
+
+$a = 10;
+$b = '10';
+
+var_dump($a == $b);
+var_dump($a === $b);
+
+
+?>
+
+
+
+
+
+?>
