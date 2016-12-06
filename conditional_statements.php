658abb31ed7af589cc5ae3220822796fe38237b6
MarioSeattle <marioseattle@live.com>
2016-12-06 11:08:08 -0800

    Comparison evaluates to true

--- conditional_statements.php
+++ conditional_statements.php
@@ -1,20 +1,20 @@
 <?php
-$name = 'mario';
-$string_one = "Learning how to display \"hello $name!\"to the screen.";
-$string_one = 'Learning how to display';
-$string_one .= '"hello';
+$name = 'Mario';
+$string_one = "Learning to display \"Hello $name!\" to the screen.\n";
+$string_one = 'Learning to display ';
+$string_one .= '"Hello ';
 $string_one .= $name;
 $string_one .= '!" to the screen.';
-$string_one = $string_one . "\n";
-
-$string_one = 'I am' . $string_one;
+//$string_one = $string_one . "\n";
+//prepend to a string
+//$string_one = 'I am ' . $string_one;
 echo $string_one;
 
 $isReady = true;
 //var_dump($isReady);
 $isReady = false;
 //var_dump($isReady);
-//One plus the string 2 ""
+
 //var_dump(1 + "2");
 
 $a = 10;
@@ -23,8 +23,5 @@ $b = '10';
 //var_dump($a == $b);
 //var_dump($a === $b);
 
-var_dump($string_one == 'Learning how to display "Hello Mario!" to the screem')
-
-
+var_dump($string_one == 'Learning to display "Hello Alena!" to the screen.');
 ?>
-
