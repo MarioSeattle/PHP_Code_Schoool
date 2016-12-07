7b8d4cbf5d958e2ab847e8318222981c646cf6a5
MarioSeattle <marioseattle@live.com>
2016-12-06 18:45:06 -0800

    Setting up exercise file with variable $day

---
+++ exercise_basics.php
@@ -0,0 +1,12 @@
+<?php
+//store each exercise in a string value
+$exercise1 = 'Display "Hello World';
+$exercise2 = 'Convert Pounds to Kilograms';
+$exercise3 = 'Covert Kilograms to Pounds';
+$exercise4 = 'Covert Miles to Kilometers';
+$exercise5 = 'Convert Kilometers to Miles';
+$exercise6 = 'Month Long string of the day';
+//Create a variable containing the day of the week
+$day = day('N');
+
+?>
