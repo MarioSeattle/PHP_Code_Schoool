ca96fa1fe11304b27d2264ed3556c4fb760bd013
MarioSeattle <marioseattle@live.com>
2016-12-06 18:59:15 -0800

    It shows the exercise for the day of the week using IF statements

--- exercise_basics.php
+++ exercise_basics.php
@@ -9,4 +9,26 @@ $exercise6 = 'Month Long string of the day';
 //Create a variable containing the day of the week
 $day = date('N');
 var_dump($day);
+//use statement to check for day of the week
+if ($day == 1){
+    echo $exercise1;
+}
+if ($day == 2){
+    echo $exercise2;
+}
+if ($day == 3){
+    echo $exercise3;
+}
+if ($day == 4){
+    echo $exercise4;
+}
+if ($day == 5){
+    echo $exercise5;
+}
+if ($day == 6){
+    echo $exercise6;
+}
+if ($day == 7){
+    echo $exercise7;
+}
 ?>
