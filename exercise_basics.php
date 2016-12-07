8f76af96db0ae62749085e1187ed826bb94e84be
MarioSeattle <marioseattle@live.com>
2016-12-06 19:03:15 -0800

    Else If to show one exercise

--- exercise_basics.php
+++ exercise_basics.php
@@ -8,27 +8,27 @@ $exercise5 = 'Convert Kilometers to Miles';
 $exercise6 = 'Month Long string of the day';
 //Create a variable containing the day of the week
 $day = date('N');
-var_dump($day);
+
 //use statement to check for day of the week
 if ($day == 1){
     echo $exercise1;
 }
-if ($day == 2){
+elseif ($day == 2){
     echo $exercise2;
 }
-if ($day == 3){
+elseif ($day == 3){
     echo $exercise3;
 }
-if ($day == 4){
+elseif ($day == 4){
     echo $exercise4;
 }
-if ($day == 5){
+elseif ($day == 5){
     echo $exercise5;
 }
-if ($day == 6){
+elseif ($day == 6){
     echo $exercise6;
 }
-if ($day == 7){
+elseif ($day == 7){
     echo $exercise7;
 }
 ?>
